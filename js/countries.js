87d647c63bac25251d7335cf366ad91ac07e6237
rakibul12212 <rakibul15-12212@diu.edu.bd>
2023-03-22 01:13:17 +0600

    s

--- js/countries.js
+++ js/countries.js
@@ -4,19 +4,45 @@ const loadCountries = () => {
         .then(data => displayCountries(data));
 }
 displayCountries = countries => {
-    console.log(countries[0]);
-    const countriesHTML = countries.map(country => getCountryHTML(country));
-    //console.log(countriesHTML[0]); // [0] disi shudu akta k dekhanor jonno
-    const container = document.getElementById('countries');
-    container.innerHTML = countriesHTML.join(' '); // empty string korar jonno  (' ') disi
+        console.log(countries[0]);
+        const countriesHTML = countries.map(country => getCountryHTML(country));
+        //console.log(countriesHTML[0]); // [0] disi shudu akta k dekhanor jonno
+        const container = document.getElementById('countries');
+        container.innerHTML = countriesHTML.join(' '); // empty string korar jonno  (' ') disi
 
-}
-const getCountryHTML = country => {
+    }
+    //orginal
+    // const getCountryHTML = country => {
+    //     return `
+    //     <div class="country">
+    //       <h2>${country.name.common}</h2>
+    //        <img src="${country.flags.png}">
+    //     </div>
+    //     `
+    // }
+
+//option-1
+// const getCountryHTML = country => {
+
+//     const { name, flags } = country;
+//     return `
+//          <div class="country">
+//            <h2>${name.common}</h2>
+//             <img src="${flags.png}">
+//          </div>
+//          `
+// }
+
+
+//option-2
+const getCountryHTML = ({ name, flags }) => { //perameter er modde dis structureing [const getCountryHTML = country => {
+
+    //  (details)   const { name, flags } = country;  "country=country={name,flags}"]
     return `
-    <div class="country">
-      <h2>${country.name.common}</h2>
-       <img src="${country.flags.png}">
-    </div>
-    `
+         <div class="country">
+           <h2>${name.common}</h2>
+            <img src="${flags.png}">
+         </div>
+         `
 }
 loadCountries();
